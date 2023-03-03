58721516a1a37fd72ad78ca2baf717b3bec9ae5e
kydycode <32301332+kydycode@users.noreply.github.com>
2023-03-03 14:21:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def send_message(message_log):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",  # The name of the OpenAI chatbot model to use
         messages=message_log,   # The conversation history up to this point, as a list of dictionaries
-        max_tokens=4096,        # The maximum number of tokens (words or subwords) in the generated response
+        max_tokens=3800,        # The maximum number of tokens (words or subwords) in the generated response
         stop=None,              # The stopping sequence for the generated response, if any (not used here)
         temperature=0.7,        # The "creativity" of the generated response (higher temperature = more creative)
     )
